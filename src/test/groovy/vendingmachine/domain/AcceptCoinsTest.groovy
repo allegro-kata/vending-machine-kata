c68d81416ec21e0a64581930fd9d37122dd3f741
Maciej Kowalski <maciej.kowalski@allegro.pl>
2014-05-07 10:29:23 +0200

    vending machine accept coins tests

--- src/test/groovy/vendingmachine/domain/AcceptCoinsTest.groovy
+++ src/test/groovy/vendingmachine/domain/AcceptCoinsTest.groovy
@@ -1,5 +1,6 @@
 package vendingmachine.domain
 
+import javafx.beans.binding.When
 import spock.lang.Ignore
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -12,25 +13,53 @@ import spock.lang.Unroll
  */
 class AcceptCoinsTest extends Specification{
 
-    @Ignore
     def "should display 'insert a coin' when ready"() {
-
+        given:
+            def machine = new VendingMachine(Stub(ProductMagazine), Stub(CoinCassette))
+            def expected = "INSERT A COIN"
+        when:
+            def display = machine.getDisplay()
+        then:
+            display == expected
     }
 
-    @Ignore
     @Unroll
     def "should accept one valid #coin and display its value as Credit"() {
-
+        given:
+            def machine = new VendingMachine(Stub(ProductMagazine), Stub(CoinCassette))
+            def expected = "CREDIT "+coin.value
+        when:
+            machine.insert(coin)
+            def display = machine.getDisplay()
+        then:
+            display == expected
+        where:
+            coin << [ Coin.NICKEL, Coin.QUARTER, Coin.DIME ]
     }
 
-    @Ignore
     def "should accept series of valid coins and should display the Credit"() {
-
+        given:
+            def machine = new VendingMachine(Stub(ProductMagazine), Stub(CoinCassette))
+            def expected = "CREDIT 0.40"
+        when:
+            machine.insert(Coin.DIME)
+            machine.insert(Coin.QUARTER)
+            machine.insert(Coin.NICKEL)
+            def display = machine.getDisplay()
+        then:
+            display == expected
     }
 
-    @Ignore
     def "should reject invalid coin and shouldn't change the Credit"() {
-
+        given:
+            def machine = new VendingMachine(Stub(ProductMagazine), Stub(CoinCassette))
+            def expected = "CREDIT 0.10"
+        when:
+            machine.insert(Coin.DIME)
+            machine.insert(Coin.PENNY)
+            def display = machine.getDisplay()
+        then:
+            display == expected
     }
 
 }
