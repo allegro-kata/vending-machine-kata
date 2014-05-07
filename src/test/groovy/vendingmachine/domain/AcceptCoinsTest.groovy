1431e2a391961bcbfb65107eb025a1cbeb23eccb
Maciej Pieszala <maciej.pieszala>
2014-05-07 10:40:42 +0200

    zadanie1

--- src/test/groovy/vendingmachine/domain/AcceptCoinsTest.groovy
+++ src/test/groovy/vendingmachine/domain/AcceptCoinsTest.groovy
@@ -1,8 +1,10 @@
 package vendingmachine.domain
 
+import com.google.common.base.Optional
 import spock.lang.Ignore
 import spock.lang.Specification
 import spock.lang.Unroll
+import spockintro.commons.Money
 
 /**
  * Tests for user story
@@ -12,23 +14,54 @@ import spock.lang.Unroll
  */
 class AcceptCoinsTest extends Specification{
 
-    @Ignore
     def "should display 'insert a coin' when ready"() {
+        given:
+        ProductMagazine magazine = Stub();
+        CoinCassette cassette = Stub();
+        def vendingMachine = new VendingMachine(magazine, cassette);
 
+        when:
+        def displayResult = vendingMachine.display
+
+        then:
+        displayResult == 'INSERT A COIN'
     }
 
-    @Ignore
     @Unroll
-    def "should accept one valid #coin and display its value as Credit"() {
+    def "should accept one valid #givenValue and display its value as Credit"() {
+        given:
+        ProductMagazine magazine = Stub();
+        CoinCassette cassette = Stub();
+        def vendingMachine = new VendingMachine(magazine, cassette);
 
+        when:
+        vendingMachine.insert(givenValue)
+        def displayResult = vendingMachine.display
+
+        then:
+        displayResult == expectedValue
+
+        where:
+        givenValue <<    [Coin.NICKEL, Coin.DIME, Coin.QUARTER]
+        expectedValue << ['CREDIT 0.05', 'CREDIT 0.10', 'CREDIT 0.25']
     }
 
-    @Ignore
     def "should accept series of valid coins and should display the Credit"() {
+        given:
+        ProductMagazine magazine = Stub();
+        CoinCassette cassette = Stub();
+        def vendingMachine = new VendingMachine(magazine, cassette);
 
+        when:
+        vendingMachine.insert(Coin.NICKEL);
+        vendingMachine.insert(Coin.DIME);
+        vendingMachine.insert(Coin.QUARTER);
+        def displayResult = vendingMachine.display
+
+        then:
+        displayResult == 'CREDIT 0.40'
     }
 
-    @Ignore
     def "should reject invalid coin and shouldn't change the Credit"() {
 
     }
